9648418c055b78a59d40f378b33f051e01d0fb48
Rawan Amjad <rawan.amjad@synergy-it.com>
2023-03-11 23:00:03 +0500

    combining city and zipcode search

--- weatherforecast/src/WeatherForecast.Presentation/src/services/weather.service.js
+++ weatherforecast/src/WeatherForecast.Presentation/src/services/weather.service.js
@@ -4,7 +4,7 @@ class WeatherService {
     this.apiKey = apiKey;
   }
 
-  async getWeather(city) {
+  async getWeatherByCity(city) {
     const params = new URLSearchParams({
       q: city,
       appid: this.apiKey,
@@ -17,7 +17,39 @@ class WeatherService {
     const data = await response.json();
     return data;
   }
+  
+  static async getWeatherByZipCode(zipCode) {
+    const params = new URLSearchParams({
+      zip: zipCode,
+      appid: this.apiKey,
+      units: "metric"
+    });
+    const response = await fetch(`${baseUrl}weather?${params}`);
+    if (!response.ok) {
+      throw new Error("Could not fetch weather data");
+    }
+    const data = await response.json();
+    return data;
+  }
 
+  async getWeather(searchText) {
+    let weatherData = null;
+    try {
+      // Try to get weather data by city name
+      weatherData = await this.getWeatherByCity(searchText);
+    } catch (error) {
+      console.log(`Error getting weather data by city name: ${error.message}`);
+  
+      try {
+        // Try to get weather data by zip code
+        weatherData = await this.getWeatherByZipCode(searchText);
+      } catch (error) {
+        console.log(`Error getting weather data by zip code: ${error.message}`);
+      }
+    }
+    
+  return weatherData;
+}
   async getForecast(city) {
     const params = new URLSearchParams({
       q: city,
